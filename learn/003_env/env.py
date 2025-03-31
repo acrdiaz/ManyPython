793146ecfcdfe4f2bd2dc537ea7506b3e1e69540
cdiaz <cdiaz@info-arch.com>
2025-03-31 08:34:51 -0400

    add lean env

---
+++ learn/003_env/env.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+print(">>>>>>>>> Hi")
+
+print("a")
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(path)
+print(path)
+
+print("b")
+api_key = os.getenv('GEMINI_API_KEY')
+print(api_key)
+
+print(">>>>>>>>> Done.")
