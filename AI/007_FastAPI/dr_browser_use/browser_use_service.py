c8bec3aa863b61bac696d2b20cae4773980f29c4
acrdiaz <acrdiaz@msn.com>
2025-05-15 15:28:17 -0400

    add validation for speech detection, and if browser-use can detect if browser is supported

--- AI/007_FastAPI/dr_browser_use/browser_use_service.py
+++ AI/007_FastAPI/dr_browser_use/browser_use_service.py
@@ -8,11 +8,6 @@ import asyncio
 import os
 
 
-BROWSER = Browser(
-    config=BrowserConfig(
-        chrome_instance_path='C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-    )
-)
 MAX_STEPS = 30
 MAX_ACTIONS_PER_STEP = 10
 
@@ -22,7 +17,51 @@ LLM_GPT = 'gpt-4o-mini'
 
 DEFAULT_MODEL = LLM_GEMINI
 
+
 class DRBrowserUseService:
+    def __init__(self, browser_name: str) -> None:
+        """Initialize the browser use service.
+
+        Args:
+            browser_name (str): Name of the browser to be used
+        """
+        if browser_name == "chrome":
+            path_32 = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+            path_64 = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+        elif browser_name == "edge":
+            path_32 = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"
+            path_64 = "C:\\Program Files\\Microsoft\\Edge\\Application\\msedge.exe"
+        else:
+            raise FileNotFoundError(f"Browser not supported: {browser_name}")
+
+        path = self.app_exits(path_32, path_64)
+
+        if path is None:
+            raise FileNotFoundError(f"Browser path file not found: {browser_name}")
+
+        self.browser = Browser(
+            config=BrowserConfig(
+                chrome_instance_path=path,
+            )
+        )
+    
+    def app_exits(self, path_32, path_64):
+        """Check if the application exists in the given paths.
+
+        Args:
+            folder_path_32 (str): Path to the 32-bit application
+            folder_path_64 (str): Path to the 64-bit application
+
+        Returns:
+            str: Path of the existing application or None
+        """
+        if os.path.exists(path_32):
+            return path_32
+        elif os.path.exists(path_64):
+            return path_64
+        else:
+            return None
+
     async def main(self, prompt = ""):  
         llm = ChatGoogleGenerativeAI(
             model=DEFAULT_MODEL,
@@ -32,7 +71,7 @@ class DRBrowserUseService:
         agent = Agent(
             task=prompt,
             llm=llm,
-            browser=BROWSER,
+            browser=self.browser,
         )
         
         await agent.run()
@@ -41,5 +80,5 @@ class DRBrowserUseService:
 
 if __name__ == "__main__":
     prompt = "open https://espanol.yahoo.com/"
-    buService = DRBrowserUseService()
+    buService = DRBrowserUseService("edge")
     asyncio.run(buService.main(prompt))
