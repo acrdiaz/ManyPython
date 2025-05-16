d806236e9270ad5623bd9bfa3b1093662f717f6a
cdiazinfoarch <cdiazinfoarch@github.com>
2025-05-16 12:43:48 -0700

    add small include routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from app.utils.dr_globals import DR_API
+# Import routers when you create them
+# from app.api.example_router import router as example_router
 
 app = FastAPI()
+
+# Register routers
+# app.include_router(example_router)
 
 @app.get("/")
 async def root():
