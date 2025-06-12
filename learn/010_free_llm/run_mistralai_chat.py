be2b9c7322de9ef93b8befbbdf97ca442518fb1b
acrdiaz <acrdiaz@msn.com>
2025-06-11 22:13:10 -0400

    add mistral models

--- learn/010_free_llm/run_mistralai_chat.py
+++ learn/010_free_llm/run_mistralai_chat.py
@@ -1,15 +1,21 @@
+# https://docs.mistral.ai/getting-started/models/models_overview/
+
 import os
 from mistralai import Mistral
 
 API_TOKEN = os.environ["MISTRAL_API_KEY"]
-MODEL = "codestral-latest"
+MODEL_CODESTRAL = "codestral-latest"
+MODEL_PIXTRAL = "pixtral-12b-2409"
+MODEL_DEVSTRAL = "devstral-small-2505"
+MODEL_DEFAULT = MODEL_DEVSTRAL
+
 PROMPT = "Write a function for fibonacci"
 
 message = [{"role": "user", "content": PROMPT}]
 
 client = Mistral(api_key=API_TOKEN)
 chat_response = client.chat.complete(
-    model=MODEL,
+    model=MODEL_DEFAULT,
     messages=message
 )
 
--- learn/010_free_llm/run_mistralai_langchain.py
+++ learn/010_free_llm/run_mistralai_langchain.py
@@ -1,14 +1,20 @@
+# https://docs.mistral.ai/getting-started/models/models_overview/
+
 import os
 from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate 
 
 API_TOKEN = os.environ["MISTRAL_API_KEY"]
-MODEL = "codestral-latest"
+MODEL_CODESTRAL = "codestral-latest"
+MODEL_PIXTRAL = "pixtral-12b-2409"
+MODEL_DEVSTRAL = "devstral-small-2505"
+MODEL_DEFAULT = MODEL_DEVSTRAL
+
 PROMPT = "Write a Python function for fibonacci"
 
 message = [("user", PROMPT)]
 
-llm = ChatMistralAI(model=MODEL, temperature=0, api_key=API_TOKEN)
+llm = ChatMistralAI(model=MODEL_DEFAULT, temperature=0, api_key=API_TOKEN)
 # llm.invoke([("user", "Write a function for fibonacci")])
 
 # # Option 1: Simple invoke and print
