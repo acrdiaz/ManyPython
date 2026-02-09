3b71c8089708df2084613fd61886b974df84bf03
acrdiaz <acrdiaz@msn.com>
2026-02-09 00:42:16 -0400

    gemini model

--- AI/005_gemini/sample.py
+++ AI/005_gemini/sample.py
@@ -5,7 +5,7 @@
 # pip show google-genai
 
 # list of models
-# https://ai.google.dev/gemini-api/docs/model
+# https://ai.google.dev/gemini-api/docs/models
 
 from google import genai
 import os
@@ -13,11 +13,17 @@ import os
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-model = "gemini-2.0-flash"
+model = "gemini-3-flash-preview"
+#        gemini-3-pro-preview     # quota exceeded
+#        gemini-3-flash-preview   # works
+#        gemini-2.5-flash         # works
+#        gemini-2.5-flash-lite    # works
+#        gemini-2.5-pro           # quota exceeded
+#        gemini-2.0-flash         # quota exceeded
 prompt = "Hi, i think it is noon now. What do you think?"
 
 response = client.models.generate_content(
-    model=model, 
+    model=model,
     contents=prompt
 )
 
